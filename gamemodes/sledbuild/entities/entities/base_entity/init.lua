9e7ebc3ec4e93cc6bfad7d2e248b383767fc0484
Jota Lanusse <jotalanusse@gmail.com>
2022-09-07 19:13:10 -0300

    Formatted a file

--- gamemodes/sledbuild/entities/entities/base_entity/init.lua
+++ gamemodes/sledbuild/entities/entities/base_entity/init.lua
@@ -1,7 +1,7 @@
 --base_entity
 
-AddCSLuaFile( "cl_init.lua" )
-AddCSLuaFile( "shared.lua" )
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
 
 include('shared.lua')
 include('outputs.lua')
@@ -12,21 +12,19 @@ include('outputs.lua')
    Desc: First function called. Use to set up your entity
 -----------------------------------------------------------]]
 function ENT:Initialize()
-		--- THIS DOESNT WORK! (stacked/duped props still collides!)
-	if self:GetClass == "prop_physics" then
-		self:SetCollisionGroup( COLLISION_GROUP_DEBRIS_TRIGGER )
-		print("ReaperSWE Debugs: Attempted to set collision groups in base_entity")
-	end
+   --- THIS DOESNT WORK! (stacked/duped props still collides!)
+   if self:GetClass() == "prop_physics" then
+      self:SetCollisionGroup(COLLISION_GROUP_DEBRIS_TRIGGER)
+      print("ReaperSWE Debugs: Attempted to set collision groups in base_entity")
+   end
 end
-
 
 --[[---------------------------------------------------------
    Name: KeyValue
    Desc: Called when a keyvalue is added to us
 -----------------------------------------------------------]]
-function ENT:KeyValue( key, value )
+function ENT:KeyValue(key, value)
 end
-
 
 --[[---------------------------------------------------------
    Name: OnRestore
@@ -36,24 +34,21 @@ end
 function ENT:OnRestore()
 end
 
-
 --[[---------------------------------------------------------
    Name: AcceptInput
    Desc: Accepts input, return true to override/accept input
 -----------------------------------------------------------]]
-function ENT:AcceptInput( name, activator, caller, data )
-	return false
+function ENT:AcceptInput(name, activator, caller, data)
+   return false
 end
-
 
 --[[---------------------------------------------------------
    Name: UpdateTransmitState
    Desc: Set the transmit state
 -----------------------------------------------------------]]
 function ENT:UpdateTransmitState()
-	return TRANSMIT_PVS
+   return TRANSMIT_PVS
 end
-
 
 --[[---------------------------------------------------------
    Name: Think
