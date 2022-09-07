97e0183cffbfc5c1a137907660590873c1573bff
Jota Lanusse <jotalanusse@gmail.com>
2022-09-07 17:58:20 -0300

    Edited how comments are marked

--- gamemodes/sledbuild/entities/entities/base_entity/init.lua
+++ gamemodes/sledbuild/entities/entities/base_entity/init.lua
@@ -7,10 +7,10 @@ include('shared.lua')
 include('outputs.lua')
 
 
-/*---------------------------------------------------------
+--[[---------------------------------------------------------
    Name: Initialize
    Desc: First function called. Use to set up your entity
----------------------------------------------------------*/
+-----------------------------------------------------------]]
 function ENT:Initialize()
 		--- THIS DOESNT WORK! (stacked/duped props still collides!)
 	if self:GetClass == "prop_physics" then
@@ -20,44 +20,44 @@ function ENT:Initialize()
 end
 
 
-/*---------------------------------------------------------
+--[[---------------------------------------------------------
    Name: KeyValue
    Desc: Called when a keyvalue is added to us
----------------------------------------------------------*/
+-----------------------------------------------------------]]
 function ENT:KeyValue( key, value )
 end
 
 
-/*---------------------------------------------------------
+--[[---------------------------------------------------------
    Name: OnRestore
    Desc: The game has just been reloaded. This is usually the right place
 		to call the GetNetworked* functions to restore the script's values.
----------------------------------------------------------*/
+-----------------------------------------------------------]]
 function ENT:OnRestore()
 end
 
 
-/*---------------------------------------------------------
+--[[---------------------------------------------------------
    Name: AcceptInput
    Desc: Accepts input, return true to override/accept input
----------------------------------------------------------*/
+-----------------------------------------------------------]]
 function ENT:AcceptInput( name, activator, caller, data )
 	return false
 end
 
 
-/*---------------------------------------------------------
+--[[---------------------------------------------------------
    Name: UpdateTransmitState
    Desc: Set the transmit state
----------------------------------------------------------*/
+-----------------------------------------------------------]]
 function ENT:UpdateTransmitState()
 	return TRANSMIT_PVS
 end
 
 
-/*---------------------------------------------------------
+--[[---------------------------------------------------------
    Name: Think
    Desc: Entity's think function. 
----------------------------------------------------------*/
+-----------------------------------------------------------]]
 function ENT:Think()
 end
