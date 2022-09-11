ed6f877f6bd8eba4b67dc9b464cdca873fa75728
Jota Lanusse <jotalanusse@gmail.com>
2022-09-11 00:21:43 -0300

    Updated how entities are counter when being searched
    - Now the # unary operator is used instead of manually counting the entities

--- gamemodes/sledbuildremastered/gamemode/sv_map.lua
+++ gamemodes/sledbuildremastered/gamemode/sv_map.lua
@@ -2,24 +2,16 @@ MAP = {}
 
 -- CountByName: Count the amount of entities with a given name
 function MAP.CountByName(name)
-	local count = 0
+	local entities = ents.FindByName(class)
 
-	for k, v in pairs(ents.FindByName(name)) do
-		count = count + 1
-	end
-
-	return count
+	return #entities
 end
 
 -- CountByClass: Count the amount of entities with a given class
 function MAP.CountByClass(class)
-	local count = 0
+	local entities = ents.FindByClass(class)
 
-	for k, v in pairs(ents.FindByClass(class)) do
-		count = count + 1
-	end
-
-	return count
+	return #entities
 end
 
 -- GatesOpen: Open the gates so the player can race
