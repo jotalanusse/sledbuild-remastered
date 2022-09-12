c3bb71ae2dc3775c3f4dbc51133f5540ab54160d
Jota Lanusse <jotalanusse@gmail.com>
2022-09-12 15:21:55 -0300

    Another UI test

--- gamemodes/sledbuildremastered/gamemode/cl_menu.lua
+++ gamemodes/sledbuildremastered/gamemode/cl_menu.lua
@@ -69,22 +69,8 @@ end
 
 function CreatePlayerList(parent)
   local listHeaders = CreateListHeaders(parent, SCOREBOARD.SIZE.HEADER_HEIGHT)
-
-  -- local lisetView = vgui.Create("DListView", parent)
-  -- lisetView:Dock(FILL)
-  -- lisetView:SetMultiSelect(false)
-  -- lisetView:SetSortable(false)
-
-  -- lisetView:AddColumn("Application")
-  -- lisetView:AddColumn("Size")
-
-  -- lisetView:AddLine("PesterChum", "2mb")
-  -- lisetView:AddLine("Lumitorch", "512kb")
-  -- lisetView:AddLine("Troj-on", "661kb")
-
-  -- lisetView.Paint = function(self, w, h)
-  --   draw.RoundedBox(0, 0, 0, w, h, Color(32, 32, 32, 191)) -- TODO: Change color
-  -- end
+  local testRow = CreateRow(parent, SCOREBOARD.SIZE.HEADER_HEIGHT, LocalPlayer())
+  testRow:SetPos(0, SCOREBOARD.SIZE.HEADER_HEIGHT)
 end
 
 function CreateListHeaders(parent, height)
@@ -98,18 +84,23 @@ function CreateListHeaders(parent, height)
     draw.RoundedBox(0, 0, 0, w, h, Color(255, 32, 32, 191)) -- TODO: Change color
   end
 
-  print(frame:GetWide())
+  local headers = {
+    { "Name", 40, }, -- TODO: Find a way to align left
+    { "Rounds", 6 },
+    { "Wins", 6 },
+    { "Podiums", 6 },
+    { "Losses", 6 },
+    { "Speed", 10 },
+    { "Top Speed", 10 },
+    { "Best Time", 10 },
+    { "Ping", 6 },
+  }
 
-  -- I don't want to touch this ever again
-  CreateHeader(frame, "Name", 40, 0)
-  CreateHeader(frame, "Rounds", 6, 40)
-  CreateHeader(frame, "Wins", 6, 46)
-  CreateHeader(frame, "Podiums", 6, 52)
-  CreateHeader(frame, "Losses", 6, 58)
-  CreateHeader(frame, "Speed", 10, 64)
-  CreateHeader(frame, "Top Speed", 10, 74)
-  CreateHeader(frame, "Best Time", 10, 84)
-  CreateHeader(frame, "Ping", 6, 94)
+  local offset = 0
+  for _, v in pairs(headers) do
+    local header = CreateHeader(frame, v[1], v[2], offset)
+    offset = offset + v[2]
+  end
 
   return frame
 end
@@ -131,7 +122,7 @@ function CreateHeader(parent, text, widthPercentage, offsetPercentage)
   return frame
 end
 
-function CreateListRow(parent, height, ply)
+function CreateRow(parent, height, ply)
   local frame = vgui.Create("DFrame", parent)
 
   DisableIntercation(frame)
@@ -139,26 +130,28 @@ function CreateListRow(parent, height, ply)
   frame:SetSize(parent:GetWide(), height)
 
   frame.Paint = function(self, w, h)
-    draw.RoundedBox(0, 0, 0, w, h, Color(255, 32, 32, 191)) -- TODO: Change color
+    draw.RoundedBox(0, 0, 0, w, h, Color(32, 32, 255, 191)) -- TODO: Change color
   end
 
-  print(frame:GetWide())
-
-  local headers = {
-    { "Name", 40 },
-    { "Rounds", 6 },
-    { "Wins", 6 },
-    { "Podiums", 6 },
-    { "Losses", 6 },
-    { "Speed", 10 },
-    { "Top Speed", 10 },
-    { "Best Time", 10 },
-    { "Ping", 6 },
+  local values = {
+    { ply:Nick(), 40},
+    { ply:GetNWInt("SBR:Rounds"), 6 },
+    { ply:GetNWInt("SBR:Wins"), 6 },
+    { ply:GetNWInt("SBR:Podiums"), 6 },
+    { ply:GetNWInt("SBR:Losses"), 6 },
+    { ply:GetNWInt("SBR:Speed"), 10 },
+    { ply:GetNWInt("SBR:TopSpeed"), 10 },
+    { ply:GetNWInt("SBR:BestTime"), 10 },
+    { ply:Ping(), 6 },
   }
 
+  local index = 1
   local offset = 0
-  for _, v in pairs(headers) do
-    local header = CreateHeader(frame, v[1], v[2], offset)
+  for _, v in pairs(values) do
+    print(v[1])
+    local rowColumn = CreateRowColumn(frame, v[1], v[2], offset)
+
+    index = index + 1
     offset = offset + v[2]
   end
 
@@ -174,7 +167,7 @@ function CreateRowColumn(parent, text, widthPercentage, offsetPercentage)
   frame:SetPos((parent:GetWide() / 100) * offsetPercentage, 0)
 
   frame.Paint = function(self, w, h)
-    draw.RoundedBox(0, 0, 0, w, h, Color(32, 32, 100, 191)) -- TODO: Change color
+    draw.RoundedBox(0, 0, 0, w, h, Color(32, 100, 32, 191)) -- TODO: Change color
   end
 
   CreateLabel(frame, text)
