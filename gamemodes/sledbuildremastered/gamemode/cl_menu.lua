99c7bc66c77f876d7de169aa97637165bf299512
Jota Lanusse <jotalanusse@gmail.com>
2022-09-13 14:21:16 -0300

    Updated scoreboard

--- gamemodes/sledbuildremastered/gamemode/cl_menu.lua
+++ gamemodes/sledbuildremastered/gamemode/cl_menu.lua
@@ -5,31 +5,30 @@ MENU = {
   },
 }
 
-local menu = menu or {}
-
-function menu:show()
+function MENU.Show()
 
   local basePanel = MENU.CreateBasePanel(MENU.SIZE.WIDTH, MENU.SIZE.HEIGHT_PERCENTAGE)
   local scoreboard = SBRD.CreateScoreboard(basePanel, SBRD.SIZE.WIDTH, SBRD.SIZE.HEIGHT_PERCENTAGE)
   local playerList = SBRD.CreatePlayerList(scoreboard)
 
-  function menu:hide()
+  function MENU.Hide()
     basePanel:Remove()
   end
 end
 
+-- ScoreboardShow: Override the default scoreboard
 function GM:ScoreboardShow()
-  menu:show()
+  MENU.Show()
 end
 
+-- ScoreboardHide: Override the default scoreboard
 function GM:ScoreboardHide()
-  menu:hide()
+  MENU.Hide()
 end
 
 -- CreateBasePanel: Create the base panel for almost all UI elements of the menu
 function MENU.CreateBasePanel(width, heightPercentage)
   local frame = vgui.Create("DFrame")
-
   HLPS.DisableFrameIntercation(frame)
 
   frame:SetSize(width, (ScrH() / 100) * heightPercentage)
@@ -37,7 +36,7 @@ function MENU.CreateBasePanel(width, heightPercentage)
   frame:MakePopup()
 
   frame.Paint = function(self, w, h)
-    draw.RoundedBox(0, 0, 0, w, h, Color(32, 32, 32, 0)) -- TODO: Base panel must be invisible
+    draw.RoundedBox(0, 0, 0, w, h, Color(32, 32, 32, 10)) -- TODO: Base panel must be invisible
   end
 
   return frame
--- gamemodes/sledbuildremastered/gamemode/cl_scoreboard.lua
+++ gamemodes/sledbuildremastered/gamemode/cl_scoreboard.lua
@@ -8,9 +8,31 @@ SBRD = {
   ROW_OPACITY = 150,
 }
 
+TEST = {
+  PLAYERS = {
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+    LocalPlayer(),
+  }
+}
+
+
+-- CreateScoreboard: Create the scoreboard
 function SBRD.CreateScoreboard(parent, width, heightPercentage)
   local frame = vgui.Create("DFrame", parent)
-
   HLPS.DisableFrameIntercation(frame)
 
   frame:SetSize(width, (parent:GetWide() / 100) * heightPercentage)
@@ -23,50 +45,52 @@ function SBRD.CreateScoreboard(parent, width, heightPercentage)
   return frame
 end
 
+-- CreatePlayerList: Create the player list (all elements)
 function SBRD.CreatePlayerList(parent)
-  local listHeaders = SBRD.CreateListHeaders(parent, SBRD.SIZE.HEADER_HEIGHT)
+  local columnWidthPercentages = { 40, 6, 6, 6, 6, 10, 10, 10, 6, }
 
-  local index = 1
-  local offset = SBRD.SIZE.HEADER_HEIGHT
+  local height = SBRD.SIZE.HEADER_HEIGHT + SBRD.SIZE.ROW_HEIGHT * #player.GetAll()
+  SBRD.CreateColumnShaders(parent, columnWidthPercentages, height)
 
-  -- TODO: Remove
-  local testPlayers = {
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-    LocalPlayer(),
-  }
+  local listHeaders = SBRD.CreateListHeaders(parent, columnWidthPercentages, SBRD.SIZE.HEADER_HEIGHT)
+  local listRows = SBRD.CreateListRows(parent, columnWidthPercentages, SBRD.SIZE.ROW_HEIGHT)
+end
 
-  for _, v in pairs(testPlayers) do
-    local opacity = SBRD.ROW_OPACITY
+function SBRD.CreateColumnShaders(parent, widthPercentages, height)
+  local frame = vgui.Create("DFrame", parent)
+  HLPS.DisableFrameIntercation(frame)
 
-    if (index % 2 == 0) then
-      opacity = opacity / 2
+  frame:SetSize(parent:GetWide(), height)
+
+  frame.Paint = function(self, w, h)
+    draw.RoundedBox(0, 0, 0, w, h, Color(32, 32, 32, 0)) -- TODO: Change color
+  end
+
+  local offsetPercentage = 0
+  for i = 1, #widthPercentages do
+    if (i % 2 == 0) then
+      SBRD.CreateColumnShader(frame, widthPercentages[i], offsetPercentage)
     end
 
-    local playerRow = SBRD.CreatePlayerRow(parent, SBRD.SIZE.ROW_HEIGHT, Color(32, 32,32, opacity), v)
-    playerRow:SetPos(0, offset)
-
-    index = index + 1
-    offset = offset + playerRow:GetTall()
+    offsetPercentage = offsetPercentage + widthPercentages[i]
   end
 end
 
-function SBRD.CreateListHeaders(parent, height)
+function SBRD.CreateColumnShader(parent, widthPercentage, offsetPercentage)
   local frame = vgui.Create("DFrame", parent)
+  HLPS.DisableFrameIntercation(frame)
 
+  frame:SetSize((parent:GetWide() / 100) * widthPercentage, parent:GetTall())
+  frame:SetPos((parent:GetWide() / 100) * offsetPercentage, 0)
+
+  frame.Paint = function(self, w, h)
+    draw.RoundedBox(0, 0, 0, w, h, Color(32, 32, 32, 100)) -- TODO: Change color
+  end
+end
+
+-- CreateListHeaders: Create the list headers
+function SBRD.CreateListHeaders(parent, widthPercentages, height)
+  local frame = vgui.Create("DFrame", parent)
   HLPS.DisableFrameIntercation(frame)
 
   frame:SetSize(parent:GetWide(), height)
@@ -76,29 +100,55 @@ function SBRD.CreateListHeaders(parent, height)
   end
 
   local headers = {
-    { "Name", 40, }, -- TODO: Find a way to align left
-    { "Rounds", 6 },
-    { "Wins", 6 },
-    { "Podiums", 6 },
-    { "Losses", 6 },
-    { "Speed", 10 },
-    { "Top Speed", 10 },
-    { "Best Time", 10 },
-    { "Ping", 6 },
+    "Name",
+    "Rounds",
+    "Wins",
+    "Podiums",
+    "Losses",
+    "Speed",
+    "Top Speed",
+    "Best Time",
+    "Ping",
   }
 
+
   local offset = 0
-  for _, v in pairs(headers) do
-    local header = SBRD.CreateHeader(frame, v[1], v[2], offset)
-    offset = offset + v[2]
+  for i = 1, #headers, 1 do
+    local header = SBRD.CreateHeader(frame, headers[i], widthPercentages[i], offset)
+
+    offset = offset + widthPercentages[i]
   end
 
   return frame
 end
 
+function SBRD.CreateListRows(parent, widthPercentages, rowHeight)
+  local rows = {}
+
+  local index = 1
+  local offset = SBRD.SIZE.HEADER_HEIGHT
+  for _, v in pairs(player.GetAll()) do
+    local opacity = SBRD.ROW_OPACITY
+
+    if (index % 2 == 0) then
+      opacity = opacity / 2
+    end
+
+    local playerRow = SBRD.CreatePlayerRow(parent, widthPercentages, rowHeight, Color(32, 32, 32, opacity), v)
+    playerRow:SetPos(0, offset)
+
+    rows[index] = playerRow
+
+    index = index + 1
+    offset = offset + rowHeight
+  end
+
+  return rows
+end
+
+-- CreateHeader: Create each specific header
 function SBRD.CreateHeader(parent, text, widthPercentage, offsetPercentage)
   local frame = vgui.Create("DFrame", parent)
-
   HLPS.DisableFrameIntercation(frame)
 
   frame:SetSize((parent:GetWide() / 100) * widthPercentage, parent:GetTall())
@@ -113,9 +163,9 @@ function SBRD.CreateHeader(parent, text, widthPercentage, offsetPercentage)
   return frame
 end
 
-function SBRD.CreatePlayerRow(parent, height, color, ply)
+-- CreatePlayerRow: Create a enw player row
+function SBRD.CreatePlayerRow(parent, widthPercentages, height, color, ply)
   local frame = vgui.Create("DFrame", parent)
-
   HLPS.DisableFrameIntercation(frame)
 
   frame:SetSize(parent:GetWide(), height)
@@ -125,32 +175,30 @@ function SBRD.CreatePlayerRow(parent, height, color, ply)
   end
 
   local values = {
-    { ply:Nick(), 40},
-    { ply:GetNWInt("SBR:Rounds"), 6 },
-    { ply:GetNWInt("SBR:Wins"), 6 },
-    { ply:GetNWInt("SBR:Podiums"), 6 },
-    { ply:GetNWInt("SBR:Losses"), 6 },
-    { ply:GetNWFloat("SBR:Speed"), 10 },
-    { ply:GetNWFloat("SBR:TopSpeed"), 10 },
-    { ply:GetNWFloat("SBR:BestTime"), 10 },
-    { ply:Ping(), 6 },
+    ply:Nick(),
+    ply:GetNWInt("SBR:Rounds"),
+    ply:GetNWInt("SBR:Wins"),
+    ply:GetNWInt("SBR:Podiums"),
+    ply:GetNWInt("SBR:Losses"),
+    ply:GetNWFloat("SBR:Speed"),
+    ply:GetNWFloat("SBR:TopSpeed"),
+    ply:GetNWFloat("SBR:BestTime"),
+    ply:Ping(),
   }
 
-  local index = 1
   local offset = 0
-  for _, v in pairs(values) do
-    local rowColumn = SBRD.CreateRowColumn(frame, v[1], v[2], offset)
+  for i = 1, #values, 1 do
+    local rowColumn = SBRD.CreateRowColumn(frame, values[i], widthPercentages[i], offset)
 
-    index = index + 1
-    offset = offset + v[2]
+    offset = offset + widthPercentages[i]
   end
 
   return frame
 end
 
+-- CreateRowColumn: Create each new row column
 function SBRD.CreateRowColumn(parent, text, widthPercentage, offsetPercentage)
   local frame = vgui.Create("DFrame", parent)
-
   HLPS.DisableFrameIntercation(frame)
 
   frame:SetSize((parent:GetWide() / 100) * widthPercentage, parent:GetTall())
@@ -165,6 +213,7 @@ function SBRD.CreateRowColumn(parent, text, widthPercentage, offsetPercentage)
   return frame
 end
 
+-- CreateLabel: Create a new label
 function SBRD.CreateLabel(parent, text)
   local label = vgui.Create("DLabel", parent)
 
