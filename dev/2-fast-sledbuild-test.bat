66576d1da3c56a38e9d5aaf25f17ebe92b9dc574
Jota Lanusse <jotalanusse@gmail.com>
2022-09-13 23:22:52 -0300

    Added dev kit

---
+++ dev/2-fast-sledbuild-test.bat
@@ -0,0 +1,4 @@
+# @echo off 
+Rem This program is to quickly test the addon in my personal machine
+
+"D:\Software\Steam\steamapps\common\GarrysMod\hl2.exe" +gamemode "sledbuild" +map "slb_snowsled_v3" -windowed -noborder -dev -console -allowdebug -hijack -steam -insecure
