786bb7d9c3535ddc67576e9b5c56b91c3184e9a1
Jota Lanusse <jotalanusse@gmail.com>
2022-09-14 23:04:41 -0300

    Added some devkit tools

--- dev/2-fast-sledbuild-test.bat
+++
@@ -1,4 +0,0 @@
-# @echo off 
-Rem This program is to quickly test the addon in my personal machine
-
-"D:\Software\Steam\steamapps\common\GarrysMod\hl2.exe" +gamemode "sledbuild" +map "slb_snowsled_v3" -windowed -noborder -dev -console -allowdebug -hijack -steam -insecure
---
+++ dev/compile-gma.bat
@@ -0,0 +1,5 @@
+# @echo off 
+Rem This program is to quickly compile the addon in my personal machine
+
+"D:\Software\Steam\steamapps\common\GarrysMod\bin\gmad.exe" create -folder "C:\Users\jotal\Documents\GitHub\sledbuild-remastered" -out "C:\Users\jotal\Documents\GitHub\sledbuild-remastered\gma\sledbuild-remastered.gma"
+pause
