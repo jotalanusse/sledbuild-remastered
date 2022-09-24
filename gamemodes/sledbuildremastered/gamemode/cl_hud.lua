626d577c0cde06b2a6ac3f10bb9289ca0c40119d
Jota Lanusse <jotalanusse@gmail.com>
2022-09-24 14:57:59 -0300

    Added initial HUD concept

--- gamemodes/sledbuildremastered/gamemode/cl_hud.lua
+++ gamemodes/sledbuildremastered/gamemode/cl_hud.lua
@@ -1,7 +1,35 @@
-HUD = {}
+HUD = {
+  DASHBOARD = {
+    MATERIAL = Material("gui/sledbuildremastered/sbr_hud.png")
+  }
+}
 
--- function HUD.Draw()
+function HUD.Paint()
+  -- local baseFrame = HUD.CreateBaseFrame()
 
+  local hud = HUD.CreateHUD()
+end
+
+hook.Add("HUDPaint", "SBR:HUD:Paint", HUD.Paint)
+
+-- CreateBaseFrame: Create the base frame for almost all UI elements of the menu
+-- function HUD.CreateBaseFrame()
+--   local frame = vgui.Create("DFrame")
+--   HLPS.DisableFrameInteraction(frame)
+
+--   frame:SetSize(ScrW(), ScrH())
+--   frame:Center()
+
+--   frame.Paint = function(self, w, h)
+--     draw.RoundedBox(0, 0, 0, w, h, COLORS.INVISIBLE)
+--   end
+
+--   return frame
 -- end
 
--- hook.Add( "HUDPaint", "SBR:HUD:Draw", HUD.Draw)
+-- CreateHUD: Create the HUD
+function HUD.CreateHUD()
+  surface.SetDrawColor(255, 255, 255, 255)
+  surface.SetMaterial(HUD.DASHBOARD.MATERIAL)
+  surface.DrawTexturedRect(ScrW() - 256, 0, 256, 256)
+end
--- gamemodes/sledbuildremastered/gamemode/cl_init.lua
+++ gamemodes/sledbuildremastered/gamemode/cl_init.lua
@@ -14,7 +14,7 @@ include("cl_helpers.lua")
 include("cl_ui.lua")
 include("cl_fonts.lua")
 
+include("cl_hud.lua")
 include("cl_scoreboard.lua")
 include("cl_menu.lua")
 
-include("cl_hud.lua")
