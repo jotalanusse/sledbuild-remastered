d3e38c906a7eefca2b920a9cdc7a2f1749cf2248
Jota Lanusse <jotalanusse@gmail.com>
2023-05-31 17:35:18 -0300

    fix: minor changes

--- gamemodes/sledbuildremastered/gamemode/server/sv_commands.lua
+++ gamemodes/sledbuildremastered/gamemode/server/sv_commands.lua
@@ -5,6 +5,7 @@ function CMDS.Help(ply)
   NET.SendGamemodeMessage(ply, "Available commands:", COLORS.MAIN)
   NET.SendGamemodeMessage(ply, "/spawn - Teleport to the spawn", COLORS.MAIN)
   NET.SendGamemodeMessage(ply, "/coinflip - Flip a coin", COLORS.MAIN)
+  Net.SendGamemodeMessage(ply, "/discord - Get the discord server link", COLORS.MAIN)
 end
 
 -- CoinFlip: Flip a coin and broadcast the result
