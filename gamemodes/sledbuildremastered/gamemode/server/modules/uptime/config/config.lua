423cc10091870b4b4db860b5fed2ccddfabe1fad
Jota Lanusse <jotalanusse@gmail.com>
2023-06-16 13:11:19 -0300

    feat: backup

--- gamemodes/sledbuildremastered/gamemode/server/modules/uptime/config/config.lua
+++ gamemodes/sledbuildremastered/gamemode/server/modules/uptime/config/config.lua
@@ -1,8 +1,6 @@
 UPTIME = {
   WARNINGS = {
-    -- FIRST = 6 * 60 * 60, -- Can't be more than 18 bits
-    -- PERIODIC = 10 * 60, -- Can't be more than 18 bits
-    FIRST = 10, -- Can't be more than 18 bits
-    PERIODIC = 8, -- Can't be more than 18 bits
+    FIRST = 6 * 60 * 60, -- Can't be more than 18 bits
+    PERIODIC = 10 * 60, -- Can't be more than 18 bits
   }
 }
