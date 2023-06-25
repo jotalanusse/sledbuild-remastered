4f0b208f524772b4f5ed2ac6f8a6c170465f1561
Jota Lanusse <jotalanusse@gmail.com>
2023-06-25 18:33:53 -0300

    feat: added the weapons module

--- src/gamemodes/sledbuildremastered/gamemode/server/sv_weapons.lua
+++ src/gamemodes/sledbuildremastered/gamemode/server/sv_weapons.lua
@@ -1,54 +1,54 @@
-WPNS = {
-  -- Loadouts a player can spawn with
-  LOADOUTS = {
-    DEFAULT = {
-      ["gmod_tool"] = true,
-      ["weapon_physgun"] = true,
-      ["weapon_physcannon"] = true,
-      ["gmod_camera"] = true,
-    },
-    ADMIN = {
-      ["gmod_tool"] = true,
-      ["weapon_physgun"] = true,
-      ["weapon_physcannon"] = true,
-      ["gmod_camera"] = true,
-      ["weapon_crowbar"] = true,
-      ["weapon_357"] = true,
-      ["weapon_bugbait"] = true,
-    },
-  }
-}
+-- WPNS = {
+--   -- Loadouts a player can spawn with
+--   LOADOUTS = {
+--     DEFAULT = {
+--       ["gmod_tool"] = true,
+--       ["weapon_physgun"] = true,
+--       ["weapon_physcannon"] = true,
+--       ["gmod_camera"] = true,
+--     },
+--     ADMIN = {
+--       ["gmod_tool"] = true,
+--       ["weapon_physgun"] = true,
+--       ["weapon_physcannon"] = true,
+--       ["gmod_camera"] = true,
+--       ["weapon_crowbar"] = true,
+--       ["weapon_357"] = true,
+--       ["weapon_bugbait"] = true,
+--     },
+--   }
+-- }
 
--- StripLoadout: Remove the player's loadout completely
-function WPNS.StripLoadout(ply)
-  ply:StripAmmo()
-  ply:StripWeapons()
-end
+-- -- StripLoadout: Remove the player's loadout completely
+-- function WPNS.StripLoadout(ply)
+--   ply:StripAmmo()
+--   ply:StripWeapons()
+-- end
 
--- SetLoadout: Set the loadout for the player
-function WPNS.SetLoadout(ply, loadout)
-  WPNS.StripLoadout(ply)
+-- -- SetLoadout: Set the loadout for the player
+-- function WPNS.SetLoadout(ply, loadout)
+--   WPNS.StripLoadout(ply)
 
-  for k, _ in pairs(loadout) do
-    ply:Give(k)
-  end
-end
+--   for k, _ in pairs(loadout) do
+--     ply:Give(k)
+--   end
+-- end
 
 -- Restrict: Restrict a player from picking up wepons outside of their loadout
-function WPNS.Restrict(ply, weapon)
-  if (ply:IsAdmin()) then
-    return true
-  end
+-- function WPNS.Restrict(ply, weapon)
+--   if (ply:IsAdmin()) then
+--     return true
+--   end
 
-  local weponsClass = weapon:GetClass()
-  if (not WPNS.LOADOUTS.DEFAULT[weponsClass]) then
-    NET.SendGamemodeMessage(ply, "You cannot pick up this weapon!")
+--   local weponsClass = weapon:GetClass()
+--   if (not WPNS.LOADOUTS.DEFAULT[weponsClass]) then
+--     NET.SendGamemodeMessage(ply, "You cannot pick up this weapon!")
 
-    return false
-  end
-end
+--     return false
+--   end
+-- end
 
-hook.Add("PlayerCanPickupWeapon", "SBR:WPNS:Restrict", WPNS.Restrict)
+-- hook.Add("PlayerCanPickupWeapon", "SBR:WPNS:Restrict", WPNS.Restrict)
 
 -- PlayerLoadout: prevent the player from spawning with their default loadout
 function GM:PlayerLoadout(ply)
