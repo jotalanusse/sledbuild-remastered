6340675d94e6268481fa7f5162244c316f713121
Jota Lanusse <jotalanusse@gmail.com>
2023-12-15 17:06:24 -0300

    feat: removed hidden spaces

--- gamemodes/sledbuildremastered/gamemode/server/sv_uptime.lua
+++ gamemodes/sledbuildremastered/gamemode/server/sv_uptime.lua
@@ -4,12 +4,12 @@ UPTIME = {
     PERIODIC = 10 * 60, -- Can't be more than 18 bits
   }
 }
-  
+
 -- Initialize: Called when we want the uptime functionality to start
 function UPTIME.Initialize()
   timer.Create("SBR:FirstUptimeWarning", UPTIME.WARNINGS.FIRST, 1, UPTIME.FirstUptimeWarning)
 end
-  
+
 hook.Add("PlayerInitialSpawn", "SBR:UPTIME:Bootstrap", UPTIME.Initialize)
 
 -- FirstUptimeWarning: Called when the first uptime warning is reached
