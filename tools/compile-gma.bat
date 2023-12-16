6083fb25eea766fd5b6df7c1616220073cabcec4
Jota Lanusse <jotalanusse@gmail.com>
2023-12-16 14:02:57 -0300

    feat: updated addon compile script

--- tools/compile-gma.bat
+++ tools/compile-gma.bat
@@ -9,4 +9,5 @@ mkdir "..\gma"
 
 echo Compiling addon...
 "G:\Software\Steam\steamapps\common\GarrysMod\bin\gmad.exe" create -folder "C:\Users\jotal\Documents\GitHub\sledbuild-remastered" -out "C:\Users\jotal\Documents\GitHub\sledbuild-remastered\gma\sledbuild-remastered.gma"
+
 pause
